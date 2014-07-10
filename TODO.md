ef624e519b9583670ba121214698c1297b69ea10
Jonathan Reyes <holostriker@gmail.com>
2014-07-10 00:32:09 -0400

    Added Actor and Spell Case Classes

---
+++ TODO.md
@@ -0,0 +1,16 @@
+Get JSON File
+Create Actor Case Class = Characters are in a list in actors
+    - [] name []- ActorData -> playerData -> playerDisplayName
+    - desc - ActorData -> playerData -> playerDescription
+    - attackType
+    - role1
+    - role2
+    - Actor stats - Dict
+    - Spells - List[Dict]
+    - Passive - Dict
+
+Create Belt Case Class
+Create Junk Case Class
+
+Make grid of characters
+Give each character own page that goes into more detail
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.libs.json._
+import play.api.libs.json._
 
+import play.api.mvc.Controller
 import play.api.mvc.Action
+
+case class Spell(spellType: String, name: String, spellCastIndicator: String,
+                 range: Float, castType: String, castDelay: Float, 
+                 coolDown: Int, globalCoolDown: Int, damage: Int, 
+                 damageRatio: Float, area: Float, duration: Int)
+
+case class Actor(name: String, desc: String, attackType: String,
+                 roles: (String, String), stats: List[Float], 
+                 spells: List[Spell])
 
 object Application extends Controller {
 
-    def index = Action {
+    def index = Action { request =>
         Ok(views.html.index())
     }
 }
