c472e350b39aab5a55bbc4a888c2b38875233acf
Jonathan Reyes <holostriker@gmail.com>
2014-07-11 02:47:31 -0400

    Added Stats Reader fixed up Champions page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,8 +14,16 @@ case class Spell(spellType: String, name: String, desc:String,
                  damage: String, damageRatio: String, area: String,
                  duration: String, spellImage: String)
 
+case class ActorStats(
+  attackDamage: String, attackDamagePerLevel: String, attackSpeed: String,
+  attackSpeedPerLevel: String, attackRange: String, armor: String, 
+  armorPerLevel: String, health: String, healthPerLevel: String,
+  spellResist: String, spellResistPerLevel: String, spellDamage: String, 
+  spellDamagePerLevel: String, speed: String
+)
+
 case class Actor(name:String, displayName: String, desc: String, attackType: String,
-                 roles: (String, String), stats: scala.collection.Map[String, JsValue], 
+                 roles: (String, String), stats: ActorStats, 
                  spells: List[Spell])
 
 object Application extends Controller {
@@ -37,15 +45,35 @@ object Application extends Controller {
     (JsPath \ "spellIconImage").read[String]
   )(Spell.apply _)
 
+  def json2Spell(json: JsValue): Spell = json.validate[Spell].get
+
+  implicit val ActorStatsReads: Reads[ActorStats]  = (
+    (JsPath \ "attackDamage").read[String] and
+    (JsPath \ "attackDamagePerLevel").read[String] and
+    (JsPath \ "attackSpeed").read[String] and
+    (JsPath \ "attackSpeedPerLevel").read[String] and
+    (JsPath \ "attackRange").read[String] and
+    (JsPath \ "armor").read[String] and
+    (JsPath \ "armorPerLevel").read[String] and
+    (JsPath \ "health").read[String] and
+    (JsPath \ "healthPerLevel").read[String] and
+    (JsPath \ "spellResist").read[String] and
+    (JsPath \ "spellResistPerLevel").read[String] and
+    (JsPath \ "spellDamage").read[String] and
+    (JsPath \ "spellDamagePerLevel").read[String] and
+    (JsPath \ "speed").read[String]
+  )(ActorStats.apply _)
+
   implicit object ActorReads extends Reads[Actor] {
     def reads(json: JsValue) = {
-      val stats = (json \ "actorStats").asInstanceOf[JsObject].value
+      val stats = (json \ "actorStats").validate[ActorStats].get
       val spells: List[Spell] = List(
         json2Spell(json \ "spell1"),
         json2Spell(json \ "spell2"),
         json2Spell(json \ "spell3"),
         json2Spell(json \ "spell4")
       )
+
       JsSuccess(Actor(
         (json \ "actorName").as[String],
         (json \ "playerData" \ "playerDisplayName").as[String],
@@ -59,7 +87,7 @@ object Application extends Controller {
     }
   }
 
-  def json2Spell(json: JsValue): Spell = json.validate[Spell].get
+  
   
 
   val jsonString = scala.io.Source.fromFile("public/files/atbp.json").getLines.mkString
@@ -70,21 +98,25 @@ object Application extends Controller {
   }
 
   def champion(name: String) = Action {
-    val charJson = json.filter(x => x.keys.last == name)(0) \ name
-    charJson.asOpt[JsValue] match {
-      case Some(thing) => {
-        (thing(0) \ "ActorData").validate[Actor] match {
-          case  s: JsSuccess[Actor] => {
-            val actor = s.get
-            Ok(views.html.champion(actor))
-          }
-          case _ => {
-            Ok(views.html.index())
+    val charJson = json.filter(x => x.keys.last == name)
+    if(charJson.isEmpty)
+      Ok(views.html.index())
+    else {
+      (charJson(0) \ name).asOpt[JsValue] match {
+        case Some(thing) => {
+          (thing(0) \ "ActorData").validate[Actor] match {
+            case  s: JsSuccess[Actor] => {
+              val actor = s.get
+              Ok(views.html.champion(actor))
+            }
+            case _ => {
+              Ok(views.html.index())
+            }
           }
         }
-      }
-      case None => {
-        Ok(views.html.index())
+        case None => {
+          Ok(views.html.index())
+        }
       }
     }
   }
