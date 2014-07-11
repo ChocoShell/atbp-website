932e2f84df1a6a0a53bcd1685001e15af56bfe53
Jonathan Reyes <holostriker@gmail.com>
2014-07-10 23:56:48 -0400

    Added Character description Page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,12 +8,13 @@ import play.api.mvc.Controller
 import play.api.mvc.Action
 import models._
 
-case class Spell (spellType: String, name: String, castIndicator: String,
-                 range: String, castType: String, castDelay: String, 
-                 coolDown: String, globalCoolDown: String, damage: String, 
-                 damageRatio: String, area: String, duration: String)
+case class Spell(spellType: String, name: String, desc:String, 
+                 castIndicator: String, range: String, castType: String,
+                 castDelay: String, coolDown: String, globalCoolDown: String,
+                 damage: String, damageRatio: String, area: String,
+                 duration: String, spellImage: String)
 
-case class Actor(name: String, desc: String, attackType: String,
+case class Actor(name:String, displayName: String, desc: String, attackType: String,
                  roles: (String, String), stats: scala.collection.Map[String, JsValue], 
                  spells: List[Spell])
 
@@ -22,6 +23,7 @@ object Application extends Controller {
   implicit val SpellReads: Reads[Spell]  = (
     (JsPath \ "spellType").read[String] and
     (JsPath \ "spellName").read[String] and
+    (JsPath \ "spellShortDescription").read[String] and
     (JsPath \ "spellCastIndicator").read[String] and
     (JsPath \ "spellRange").read[String] and
     (JsPath \ "castType").read[String] and
@@ -31,7 +33,8 @@ object Application extends Controller {
     (JsPath \ "damage").read[String] and
     (JsPath \ "damageRatio").read[String] and
     (JsPath \ "spellArea").read[String] and
-    (JsPath \ "spellDuration").read[String]
+    (JsPath \ "spellDuration").read[String] and
+    (JsPath \ "spellIconImage").read[String]
   )(Spell.apply _)
 
   implicit object ActorReads extends Reads[Actor] {
@@ -44,6 +47,7 @@ object Application extends Controller {
         json2Spell(json \ "spell4")
       )
       JsSuccess(Actor(
+        (json \ "actorName").as[String],
         (json \ "playerData" \ "playerDisplayName").as[String],
         (json \ "playerData" \ "playerDescription").as[String],
         (json \ "attackType").as[String],
@@ -59,14 +63,15 @@ object Application extends Controller {
   
 
   val jsonString = scala.io.Source.fromFile("public/files/atbp.json").getLines.mkString
-  val json: JsValue = (Json.parse(jsonString) \ "Actors")(0)
+  val json: List[JsObject] = (Json.parse(jsonString) \ "Actors").as[List[JsObject]]
 
   def index = Action { request =>
     Ok(views.html.index())
   }
 
   def champion(name: String) = Action {
-    (json \ name).asOpt[JsValue] match {
+    val charJson = json.filter(x => x.keys.last == name)(0) \ name
+    charJson.asOpt[JsValue] match {
       case Some(thing) => {
         (thing(0) \ "ActorData").validate[Actor] match {
           case  s: JsSuccess[Actor] => {
