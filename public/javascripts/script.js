8bb9e884c1983f5289233de3eb98303976cbafd8
Jonathan Reyes <holostriker@gmail.com>
2014-07-18 07:11:17 -0400

    Adding Angular to Project

---
+++ public/javascripts/script.js
@@ -0,0 +1,15 @@
+(function() {
+  var myApp = angular.module('myApp', []);
+
+  myApp.controller('MainController', function() {
+    this.message = 'Whatup';
+  });
+
+  myApp.controller('BeltController', function(){
+
+  });
+
+})();
+
+
+  
