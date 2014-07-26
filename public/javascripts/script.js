09536097b5cae7dbeebbe7a61a43a33fab173d62
Jonathan Reyes <holostriker@gmail.com>
2014-07-25 23:26:21 -0400

    Fixed twitch search url

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -54,7 +54,7 @@
     $scope.streams = [];
     $scope.init = function () {
       $scope.streams = [];
-      $http.jsonp("https://api.twitch.tv/kraken/search/streams?q=%22adventure%20time%20battle%20party%22&callback=JSON_CALLBACK").success(
+      $http.jsonp("https://api.twitch.tv/kraken/search/streams?q=%22adventure%20time%22&callback=JSON_CALLBACK").success(
         function(data) {
           $scope.streams = data.streams;
         }).error(function(data) {});
